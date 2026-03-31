f73f65c4d2fa95eefc7f6a8c403a4afebfa62f47
Reinier1313 <reinier231@gmail.com>
2026-03-31 11:10:07 +0800

    Added Mobile feature

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,5 @@
+import withPWA from 'next-pwa';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
@@ -8,4 +10,35 @@ const nextConfig = {
   },
 }
 
-export default nextConfig
+const withPWAConfig = withPWA({
+  dest: 'public',
+  register: true,
+  skipWaiting: false,
+  disable: process.env.NODE_ENV === 'development',
+  runtimeCaching: [
+    {
+      urlPattern: /^https:\/\/fonts\.(?:gstatic)\.com\/.*/i,
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'google-fonts-webfonts',
+        expiration: {
+          maxEntries: 4,
+          maxAgeSeconds: 365 * 24 * 60 * 60 // 1 year
+        }
+      }
+    },
+    {
+      urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+      handler: 'StaleWhileRevalidate',
+      options: {
+        cacheName: 'google-fonts-stylesheets',
+        expiration: {
+          maxEntries: 4,
+          maxAgeSeconds: 7 * 24 * 60 * 60 // 1 week
+        }
+      }
+    }
+  ]
+});
+
+export default withPWAConfig(nextConfig)
